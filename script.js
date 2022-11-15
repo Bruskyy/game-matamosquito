e5dad7ffc26d11faeccfcbb36c52c3343e6c1911
Bruskyy <bruschivitor@gmail.com>
2022-11-15 11:44:57 -0300

    iniciando script js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.getElementsByTagName("html")[0].style.cursor = "url('imagens/mata_mosca.png'), auto";
+document.getElementsByTagName('html')[0].style.cursor = "url('imagens/mata_mosca.png'), auto";
 
+var altura = 0
+var largura = 0
 
+function pathingGame() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+pathingGame()
+
+function randomPosition() {
+    var posicaoX = Math.floor(Math.random() * largura)
+    var posicaoY = Math.floor(Math.random() * altura)
+    console.log(posicaoX, posicaoY)
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    document.body.appendChild(mosquito)
+    mosquito.className = 'mosquito'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+}
