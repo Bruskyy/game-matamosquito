05071ce32807a55b03a7138a175913930dfcd2fb
Bruskyy <bruschivitor@gmail.com>
2022-11-17 08:52:17 -0300

    commit

--- script.js
+++ script.js
@@ -13,15 +13,48 @@ function pathingGame() {
 pathingGame()
 
 function randomPosition() {
-    var posicaoX = Math.floor(Math.random() * largura)
-    var posicaoY = Math.floor(Math.random() * altura)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
     console.log(posicaoX, posicaoY)
+
 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
     document.body.appendChild(mosquito)
-    mosquito.className = 'mosquito'
+    mosquito.className = randomSize() + ' ' + randomSide()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+
+}
+
+function randomSize() {
+
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito'
+        case 1:
+            return 'mosquito1'
+        case 2:
+            return 'mosquito2'
+
+    }
+}
+
+function randomSide() {
+
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+
+    }
 }
